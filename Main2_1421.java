e33d4ca43c77ae4f7a6731226a05b714d8b3beeb
aiue01212 <aiue85605@gmail.com>
2026-02-05 17:49:23 +0900

    20260205

--- Main2_1421.java
+++ Main2_1421.java
@@ -1,21 +1,58 @@
-import java.util.*;
+import java.io.*;
 
 public class Main2_1421 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        String T = sc.nextLine();
-        String P = sc.nextLine();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = T.length();
-        int m = P.length();
+        String T = br.readLine();
+        String P = br.readLine();
 
-        for (int i = 0; i <= n - m; i++) {
-            if (T.substring(i, i + m).equals(P)) {
-                System.out.println(i);
+        int[] lps = makeLPS(P);
+        StringBuilder sb = new StringBuilder();
+
+        int i = 0, j = 0;
+
+        while (i < T.length()) {
+            if (T.charAt(i) == P.charAt(j)) {
+                i++;
+                j++;
+
+                if (j == P.length()) {
+                    sb.append(i - j).append('\n');
+                    j = lps[j - 1];
+                }
+            } else {
+                if (j > 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
             }
         }
 
-        sc.close();
+        System.out.print(sb.toString());
+    }
+
+    static int[] makeLPS(String p) {
+        int[] lps = new int[p.length()];
+        int len = 0;
+        int i = 1;
+
+        while (i < p.length()) {
+            if (p.charAt(i) == p.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len > 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps;
     }
 }
